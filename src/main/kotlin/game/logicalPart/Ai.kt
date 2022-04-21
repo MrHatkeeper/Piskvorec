83bfa4ff678bebd414cb2799d454a581351746d0
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2022-04-21 20:10:52 +0200

    some small changes

--- src/main/kotlin/game/logicalPart/Ai.kt
+++ src/main/kotlin/game/logicalPart/Ai.kt
@@ -2,8 +2,12 @@ package game.logicalPart
 
 class Ai {
     var lastMove = IntArray(2)
-    /*fun makeMove(grid: Array<IntArray>, symbol: Int): Array<IntArray> {
 
-    }*/
+    fun makeMove(grid: Array<IntArray>, symbol: Int, lastPreviousMove: IntArray): Array<IntArray> {
+        val cooX = (lastPreviousMove[1] - 1..lastPreviousMove[1] + 1).random()
+        val cooY = (lastPreviousMove[0] - 1..lastPreviousMove[0] + 1).random()
 
+        grid[cooY][cooX] = symbol
+        return grid
+    }
 }
